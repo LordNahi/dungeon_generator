f61c4d2bd5563cfc88065bdc8552b68f1b57234f
Nathan Nahi <nathannahisteam@gmail.com>
2017-02-19 23:34:49 +1300

    added new build script

---
+++ build.js
@@ -0,0 +1,62 @@
+var fs = require("fs");
+var path = require("path");
+
+function exec(cmd) {
+    try {
+        var output = String(require("child_process").execSync(cmd)).trim();
+        return output;
+    } catch (err) {
+        console.log(String(err.stdout));
+        throw err;
+    }
+}
+
+function buildProject() {
+    exec("tsc -p .");
+}
+
+function copy(from, to) {
+    
+}
+
+function walk(dir) {
+    var results = [];
+    list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) results = results.concat(walk(file));
+        else results.push(file);
+    })
+    return results;
+}
+
+function pushArtifacts(artifacts) {
+    makeDir("");
+    artifacts.forEach((file) => {
+        console.log("Pushing " + file);
+        pushFile(file);
+    });
+}
+
+function deploy(codeOnly) {
+    var cordovaDir = "";
+
+    if (!cordovaDir === "cordova") {
+        exec("xcopy assets/* " + cordovaDir + "/assets/");
+        exec("xcopy lib/* " + cordovaDir + "/lib/");
+    } else {
+        console.log("Cordova directory not specified, edit in build.js ...");
+    }
+}
+
+var option = process.argv[2];
+if (option) {
+    switch (option) {
+        case "--build":
+            buildProject();
+        break;
+
+        // Add new cases as required ...
+    }
+}
